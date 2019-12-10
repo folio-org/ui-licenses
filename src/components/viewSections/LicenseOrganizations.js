0e66cd6ff198d47ad5940706fbefc633466598a7
EthanFreestone <ethan.freestone@k-int.co.uk>
2019-12-10 09:31:28 +0000

    undid stupid git mistake

--- src/components/viewSections/LicenseOrganizations.js
+++ src/components/viewSections/LicenseOrganizations.js
@@ -3,7 +3,7 @@ import PropTypes from 'prop-types';
 import { get } from 'lodash';
 import { FormattedMessage } from 'react-intl';
 import { Link } from 'react-router-dom';
-import { ViewOrganizationCard } from '@folio/stripes-erm-components';
+import { ViewOrganizationCard } from '@folio/stripes/stripes-erm-components';
 import { Accordion, Badge, Icon, Layout } from '@folio/stripes/components';
 import { AppIcon } from '@folio/stripes/core';
 
