a6ff90d303b2f1d45d1b73cdb07b7dffdb2d2b16
Monireh Rasouli <83644557+MonireRasouli@users.noreply.github.com>
2022-01-18 12:09:34 +0000

    ERM-1448, Add test coverage for <AmendmentFormInfo.js>

--- src/components/formSections/index.js
+++ src/components/formSections/index.js
@@ -1,5 +1,3 @@
-export { default as AmendmentFormInfo } from './AmendmentFormInfo';
-
 export { default as LicenseFormInfo } from './LicenseFormInfo';
 export { default as LicenseFormInternalContacts } from './LicenseFormInternalContacts';
 export { default as LicenseFormOrganizations } from './LicenseFormOrganizations';
