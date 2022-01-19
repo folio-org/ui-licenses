9411ab8cedde785a031037f71c12044c665af753
Monireh Rasouli <83644557+MonireRasouli@users.noreply.github.com>
2022-01-19 14:38:54 +0000

    ERM-1450, Add test coverage for <FormSupplementaryDocs.js> (#520)

---
+++ src/components/formSections/FormSupplementaryDocs/FormSupplementaryDocs.test.js
@@ -0,0 +1,77 @@
+import React from 'react';
+import '@folio/stripes-erm-components/test/jest/__mock__';
+import { renderWithIntl, TestForm } from '@folio/stripes-erm-components/test/jest/helpers';
+import { Accordion } from '@folio/stripes-testing';
+import { data, initialValues } from './testResources';
+import translationsProperties from '../../../../test/helpers';
+import FormSupplementaryDocs from './FormSupplementaryDocs';
+
+jest.mock('@folio/stripes-erm-components', () => ({
+    ...jest.requireActual('@folio/stripes-erm-components'),
+    DocumentsFieldArray: () => <div>DocumentsFieldArray</div>,
+  }));
+
+const onSubmitMock = jest.fn();
+const onDownloadFileMock = jest.fn();
+const onUploadFileMock = jest.fn();
+const onCloseMock = jest.fn();
+
+describe('FormSupplementaryDocs', () => {
+  let renderComponent;
+  describe('with initialValues', () => {
+    beforeEach(() => {
+      renderComponent = renderWithIntl(
+        <TestForm onSubmit={onSubmitMock}>
+          <FormSupplementaryDocs
+            data={data}
+            handlers={{
+              onClose: onCloseMock,
+              onDownloadFile: onDownloadFileMock,
+              onUploadFile: onUploadFileMock
+            }}
+            id="licenseFormSupplementaryDocs"
+            initialValues={initialValues}
+          />
+        </TestForm>,
+        translationsProperties
+      );
+    });
+
+    test('renders the Supplementary documents Accordion', async () => {
+        await Accordion('Supplementary documents').exists();
+    });
+
+    it('renders the DocumentsFieldArray component', () => {
+      const { getByText } = renderComponent;
+      expect(getByText('DocumentsFieldArray')).toBeInTheDocument();
+    });
+  });
+
+  describe('with no initialValues', () => {
+    beforeEach(() => {
+      renderComponent = renderWithIntl(
+        <TestForm onSubmit={onSubmitMock}>
+          <FormSupplementaryDocs
+            data={data}
+            handlers={{
+              onClose: onCloseMock,
+              onDownloadFile: onDownloadFileMock,
+              onUploadFile: onUploadFileMock
+            }}
+            id="licenseFormSupplementaryDocs"
+          />
+        </TestForm>,
+        translationsProperties
+      );
+    });
+
+    test('renders the Supplementary documents Accordion', async () => {
+        await Accordion('Supplementary documents').exists();
+    });
+
+    it('renders the DocumentsFieldArray component', () => {
+      const { getByText } = renderComponent;
+      expect(getByText('DocumentsFieldArray')).toBeInTheDocument();
+    });
+  });
+});
