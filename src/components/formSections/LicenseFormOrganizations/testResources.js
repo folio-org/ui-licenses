e95f11d68dd1b4a590f3d737667741344e32daa0
Monireh Rasouli <83644557+MonireRasouli@users.noreply.github.com>
2022-01-19 14:40:33 +0000

    ERM-1454, Add test coverage for <LicenseFormOrganizations.js> (#518)

---
+++ src/components/formSections/LicenseFormOrganizations/testResources.js
@@ -0,0 +1,89 @@
+const data = {
+  'orgRoleValues': [{
+    'id': '2c91809d7e7001a5017e70085dd20001',
+    'value': 'licensor',
+    'label': 'Licensor',
+    'owner': {
+      'id': '2c91809d7e7001a5017e70085d7e0000',
+      'desc': 'LicenseOrg.Role',
+      'internal': false
+    }
+  }]
+};
+const initialValues = {
+  'id': '1242f622-0bc3-43ec-9ff1-8384a68221bf',
+  'endDateSemantics': {
+    'id': '2c91809d7e7001a5017e70085e320012',
+    'value': 'explicit',
+    'label': 'Explicit',
+    'owner': {
+      'id': '2c91809d7e7001a5017e70085e2e0011',
+      'desc': 'License.EndDateSemantics',
+      'internal': true
+    }
+  },
+  'dateCreated': '2022-01-19T09:59:09Z',
+  'customProperties': {
+    'authorisedUsers': [
+      '{_delete: true}'
+    ],
+    'remoteAccess': [
+      '{_delete: true}'
+    ],
+    'illElectronic': [
+      '{_delete: true}'
+    ],
+    'copyDigital': [
+      '{_delete: true}'
+    ],
+    'copyPrint': [
+      '{_delete: true}'
+    ],
+    'concurrentAccess': [
+      '{_delete: true}'
+    ],
+    'illPrint': [
+      '{_delete: true}'
+    ],
+    'illSecureElectronic': [
+      '{_delete: true}'
+    ],
+    'reservesElectronic': [
+      '{_delete: true}'
+    ],
+    'coursePackElectronic': [
+      '{_delete: true}'
+    ],
+    'coursePackPrint': [
+      '{_delete: true}'
+    ],
+    'walkInAccess': [
+      '{_delete: true}'
+    ]
+  },
+  'contacts': [{
+    'id': '2c91809d7e7001a5017e71c74d26003d',
+    'owner': '{id: "1242f622-0bc3-43ec-9ff1-8384a68221bf"}',
+    'role': 'erm_librarian',
+    'user': 'ca4e1156-0629-4f1e-a8c2-e22176b8d02a'
+  }],
+  'tags': '[]',
+  'lastUpdated': '2022-01-19T09:59:53Z',
+  'docs': '[]',
+  'name': 'MR License',
+  'status': 'active',
+  'supplementaryDocs': '[]',
+  'description': 'licences description',
+  'startDate': '2022-01-02',
+  'endDate': '2022-01-30',
+  'openEnded': false,
+  'amendments': '[]',
+  'orgs': '[]',
+  'type': 'local',
+  'alternateNames': '[]'
+};
+
+export {
+  data,
+  initialValues
+};
