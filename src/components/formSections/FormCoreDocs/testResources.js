ec31d69eb151f41bfb95fbe8c12f724d53c0a2ad
Monireh Rasouli <83644557+MonireRasouli@users.noreply.github.com>
2022-01-19 14:25:01 +0000

    ERM-1449, Add test coverage for <FormCoreDocs.js> (#517)

---
+++ src/components/formSections/FormCoreDocs/testResources.js
@@ -0,0 +1,111 @@
+const initialValues = {
+  'id': '9bc32c30-ffc6-4970-b741-95e7eddc3afa',
+  'endDateSemantics': {
+    'id': '2c91809d7e6adbbf017e6ae2dc52000b',
+    'value': 'explicit',
+    'label': 'Explicit',
+    'owner': {
+      'id': '2c91809d7e6adbbf017e6ae2dc4f000a',
+      'desc': 'License.EndDateSemantics',
+      'internal': true
+    }
+  },
+  'dateCreated': '2022-01-18T09:32:20Z',
+  'customProperties': {
+    'authorisedUsers': [
+      '{_delete: true}'
+    ],
+    'remoteAccess': [
+      '{_delete: true}'
+    ],
+    'illElectronic': [
+      '{_delete: true}'
+    ],
+    'copyDigital': [
+      '{_delete: true}'
+    ],
+    'copyPrint': [
+      '{_delete: true}'
+    ],
+    'concurrentAccess': [
+      '{_delete: true}'
+    ],
+    'illPrint': [
+      '{_delete: true}'
+    ],
+    'illSecureElectronic': [
+      '{_delete: true}'
+    ],
+    'reservesElectronic': [
+      '{_delete: true}'
+    ],
+    'coursePackElectronic': [
+      '{_delete: true}'
+    ],
+    'coursePackPrint': [
+      '{_delete: true}'
+    ],
+    'walkInAccess': [
+      '{_delete: true}'
+    ]
+  },
+  'contacts': '[]',
+  'tags': '[]',
+  'lastUpdated': '2022-01-18T12:40:14Z',
+  'docs': [{
+    'id': 'd7042b02-3c7e-48c6-888e-e3c9d005b287',
+    'dateCreated': '2022-01-18T12:40:14Z',
+    'lastUpdated': '2022-01-18T12:40:14Z',
+    'fileUpload': '{contentType: "image/png", id: "14d6f399-20bc-44ee-…}',
+    'name': 'Core Docs test',
+    'note': 'note'
+  }],
+  'name': 'MR License',
+  'status': 'active',
+  'supplementaryDocs': '[]',
+  'description': 'description',
+  'startDate': '2022-01-12',
+  'endDate': '2022-01-27',
+  'openEnded': false,
+  'amendments': [{
+    'id': '69a0041c-a246-4944-ab6c-3cf3d77df064',
+    'endDateSemantics': {
+      'id': '2c91809d7e6adbbf017e6ae2dc5a000c',
+      'value': 'open_ended',
+      'label': 'Open ended',
+      'owner': {
+        'id': '2c91809d7e6adbbf017e6ae2dc4f000a',
+        'desc': 'License.EndDateSemantics',
+        'internal': true
+      }
+    },
+    'dateCreated': '2022-01-18T09:32:42Z',
+    'customProperties': '{}',
+    'contacts': '[]',
+    'tags': '[]',
+    'lastUpdated': '2022-01-18T12:40:14Z',
+    'docs': '[]',
+    'name': 'Amendment Test',
+    'status': {
+      'id': '2c91809d7e6adbbf017e6ae2dc7c0011',
+      'value': 'active',
+      'label': 'Active',
+      'owner': {
+        'id': '2c91809d7e6adbbf017e6ae2dc69000e',
+        'desc': 'License.Status',
+        'internal': true
+      }
+    },
+    'supplementaryDocs': '[]',
+    'startDate': '2022-01-28',
+    'endDate': '2022-01-02',
+    'openEnded': true
+  }],
+  'orgs': '[]',
+  'type': 'national',
+  'alternateNames': '[]'
+};
+
+export default initialValues;
+
+
