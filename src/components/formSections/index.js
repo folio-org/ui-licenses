05c4400b2c37b973af65fb6bf4719661f0d28f86
Monireh Rasouli <83644557+MonireRasouli@users.noreply.github.com>
2022-01-19 13:00:45 +0000

    update

--- src/components/formSections/index.js
+++ src/components/formSections/index.js
@@ -1,3 +1,5 @@
+export { default as AmendmentFormInfo } from './AmendmentFormInfo';
+
 export { default as LicenseFormInfo } from './LicenseFormInfo';
 export { default as LicenseFormInternalContacts } from './LicenseFormInternalContacts';
 export { default as LicenseFormOrganizations } from './LicenseFormOrganizations';
