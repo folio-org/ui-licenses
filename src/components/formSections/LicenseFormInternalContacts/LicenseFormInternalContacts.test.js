a1447aa6a50cc729c6a74245b51a1b0c54032188
Monireh Rasouli <83644557+MonireRasouli@users.noreply.github.com>
2022-01-19 10:22:22 +0000

    ERM-1453, Add test coverage for <LicenseFormInternalContacts.js>

---
+++ src/components/formSections/LicenseFormInternalContacts/LicenseFormInternalContacts.test.js
@@ -0,0 +1,86 @@
+import React from 'react';
+import '@folio/stripes-erm-components/test/jest/__mock__';
+import { renderWithIntl, TestForm } from '@folio/stripes-erm-components/test/jest/helpers';
+import { screen } from '@testing-library/react';
+import { Accordion } from '@folio/stripes-testing';
+import { data, mutators, values, initialValues } from './testResources';
+import translationsProperties from '../../../../test/helpers';
+import LicenseFormInternalContacts from './LicenseFormInternalContacts';
+
+jest.mock('@folio/stripes-erm-components', () => ({
+    ...jest.requireActual('@folio/stripes-erm-components'),
+    InternalContactsFieldArray: () => <div>InternalContactsFieldArray</div>,
+  }));
+
+const onSubmitMock = jest.fn();
+const onDownloadFileMock = jest.fn();
+const onUploadFileMock = jest.fn();
+const onCloseMock = jest.fn();
+const onToggleMock = jest.fn();
+
+describe('LicenseFormInternalContacts', () => {
+  let renderComponent;
+  describe('with initialValues', () => {
+    beforeEach(() => {
+      renderComponent = renderWithIntl(
+        <TestForm onSubmit={onSubmitMock}>
+          <LicenseFormInternalContacts
+            data={data}
+            handlers={{
+              onClose: onCloseMock,
+              onDownloadFile: onDownloadFileMock,
+              onUploadFile: onUploadFileMock
+            }}
+            id="licenseFormInternalContacts"
+            initialValues={initialValues}
+            mutators={mutators}
+            onToggle={onToggleMock}
+            values={values}
+          />
+        </TestForm>,
+        translationsProperties
+      );
+    });
+
+    test('renders the Internal contacts Accordion', async () => {
+        await Accordion('Internal contacts').exists();
+    });
+
+    it('renders the InternalContactsFieldArray component', () => {
+      const { getByText } = renderComponent;
+      expect(getByText('InternalContactsFieldArray')).toBeInTheDocument();
+    });
+  });
+
+  describe('with no initialValues', () => {
+    beforeEach(() => {
+      renderComponent = renderWithIntl(
+        <TestForm onSubmit={onSubmitMock}>
+          <LicenseFormInternalContacts
+            data={data}
+            handlers={{
+              onClose: onCloseMock,
+              onDownloadFile: onDownloadFileMock,
+              onUploadFile: onUploadFileMock
+            }}
+            id="licenseFormInternalContacts"
+            mutators={mutators}
+            onToggle={onToggleMock}
+            values={values}
+          />
+        </TestForm>,
+        translationsProperties
+      );
+    });
+
+    test('renders the Internal contacts Accordion', async () => {
+        screen.debug();
+        await Accordion('Internal contacts').exists();
+    });
+
+    it('renders the InternalContactsFieldArray component', () => {
+      const { getByText } = renderComponent;
+      expect(getByText('InternalContactsFieldArray')).toBeInTheDocument();
+    });
+  });
+});
