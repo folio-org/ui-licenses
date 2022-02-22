d23f11a9f28fc5425735eeb4646e44d29ce2e7d1
Monireh Rasouli <83644557+MonireRasouli@users.noreply.github.com>
2022-02-22 11:55:02 +0000

    Add test coverage for ui-licenses <ViewAmendmentRoute>

---
+++ src/routes/ViewAmendmentRoute/ViewAmendmentRoute.test.js
@@ -0,0 +1,160 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import '@folio/stripes-erm-components/test/jest/__mock__';
+import { renderWithIntl } from '@folio/stripes-erm-components/test/jest/helpers';
+import { MemoryRouter } from 'react-router-dom';
+import { noop } from 'lodash';
+import { Button } from '@folio/stripes/components';
+import { Button as ButtonInteractor } from '@folio/stripes-testing';
+import { handlers, resources, okapi } from './testResources';
+import translationsProperties from '../../../test/helpers';
+import ViewAmendmentRoute from './ViewAmendmentRoute';
+
+const CloseButton = (props) => {
+  return <Button onClick={props.handlers.onClose}>CloseButton</Button>;
+};
+
+const DeleteButton = (props) => {
+  return <Button onClick={props.handlers.onDelete}>DeleteButton</Button>;
+};
+
+const CloneButton = (props) => {
+  return <Button onClick={props.handlers.onClone}>CloneButton</Button>;
+};
+
+const EditAmendmentButton = (props) => {
+  return <Button onClick={props.handlers.onEditAmendment}>EditAmendmentButton</Button>;
+};
+
+const ViewAmendmentButton = (props) => {
+  return <Button onClick={props.handlers.onAmendmentClick}>ViewAmendmentButton</Button>;
+};
+
+CloseButton.propTypes = {
+  handlers: PropTypes.shape({
+    onClose: PropTypes.func,
+  }),
+};
+
+DeleteButton.propTypes = {
+  handlers: PropTypes.shape({
+    onDelete: PropTypes.func,
+  }),
+};
+
+CloneButton.propTypes = {
+  handlers: PropTypes.shape({
+    onClone: PropTypes.func,
+  }),
+};
+
+EditAmendmentButton.propTypes = {
+  handlers: PropTypes.shape({
+    onEditAmendment: PropTypes.func,
+  }),
+};
+
+ViewAmendmentButton.propTypes = {
+  handlers: PropTypes.shape({
+    onAmendmentClick: PropTypes.func,
+  }),
+};
+
+const historyPushMock = jest.fn();
+const hasPermMock = jest.fn();
+
+jest.mock('../../components/Amendment', () => {
+  return (props) => (
+    <div>
+      <div>Amendment</div>
+      <CloseButton {...props} />
+      <DeleteButton {...props} />
+      <CloneButton {...props} />
+      <EditAmendmentButton {...props} />
+      <ViewAmendmentButton {...props} />
+    </div>
+  );
+});
+
+const data = {
+  handlers,
+  history: {
+    push: historyPushMock
+  },
+  location: {
+    search: ''
+  },
+  mutator: {
+    license: {
+      PUT: noop
+    }
+  },
+  match: {
+    params: {
+      amendmentId: 'd758b0e4-1bcd-43b6-a6aa-99a12f7a402e…',
+      id: '766aea02-9071-4cec-a6ef-3ed8208194de'
+    },
+  },
+  resources: {
+    resources
+  },
+  stripes: {
+    hasPerm: hasPermMock,
+    okapi
+  }
+};
+
+describe('ViewAmendmentRoute', () => {
+  describe('rendering the ViewAmendmentRoute', () => {
+    let renderComponent;
+    beforeEach(() => {
+      renderComponent = renderWithIntl(
+        <MemoryRouter>
+          <ViewAmendmentRoute {...data} />
+        </MemoryRouter>,
+        translationsProperties
+      );
+    });
+
+    test('renders the Amendment component', () => {
+      const { getByText } = renderComponent;
+      expect(getByText('Amendment')).toBeInTheDocument();
+    });
+
+    test('renders the CloseButton ', () => {
+      const { getByText } = renderComponent;
+      expect(getByText('CloseButton')).toBeInTheDocument();
+    });
+
+    test('triggers the CloseButton callback', async () => {
+      await ButtonInteractor('CloseButton').click();
+      expect(historyPushMock).toHaveBeenCalled();
+    });
+
+    test('renders the DeleteButton ', () => {
+      const { getByText } = renderComponent;
+      expect(getByText('DeleteButton')).toBeInTheDocument();
+    });
+
+    test('renders the CloneButton ', () => {
+      const { getByText } = renderComponent;
+      expect(getByText('CloneButton')).toBeInTheDocument();
+    });
+
+    test('renders the EditAmendmentButton ', () => {
+      const { getByText } = renderComponent;
+      expect(getByText('EditAmendmentButton')).toBeInTheDocument();
+    });
+
+
+    test('renders the ViewAmendmentButton ', () => {
+      const { getByText } = renderComponent;
+      expect(getByText('ViewAmendmentButton')).toBeInTheDocument();
+    });
+
+    test('triggers the ViewAmendmentButton callback', async () => {
+      await ButtonInteractor('ViewAmendmentButton').click();
+      expect(historyPushMock).toHaveBeenCalled();
+    });
+  });
+});
---
+++ src/routes/ViewAmendmentRoute/testResources.js
@@ -0,0 +1,88 @@
+const handlers = {
+  'onDownloadFile': 'ƒ () {}',
+  'onUploadFile': 'ƒ () {}'
+};
+
+const history = {
+  'length': 16,
+  'action': 'PUSH',
+  'location': '{hash: "", key: "ki64v4", pathname: "/licenses/766a…}',
+  'createHref': 'ƒ createHref() {}',
+  'push': 'ƒ push() {}',
+  'replace': 'ƒ replace() {}',
+  'go': 'ƒ go() {}',
+  'goBack': 'ƒ goBack() {}',
+  'goForward': 'ƒ goForward() {}',
+  'block': 'ƒ block() {}',
+  'listen': 'ƒ listen() {}'
+};
+
+const location = {
+  'pathname': '/licenses/766aea02-9071-4cec-a6ef-3ed8208194de/amendments/d758b0e4-1bcd-43b6-a6aa-99a12f7a402e',
+  'search': '?filters=status.active&sort=name',
+  'hash': '',
+  'key': 'ki64v4'
+};
+
+const match = {
+  'path': '/licenses/:id/amendments/:amendmentId',
+  'url': '/licenses/766aea02-9071-4cec-a6ef-3ed8208194de/amendments/d758b0e4-1bcd-43b6-a6aa-99a12f7a402e',
+  'isExact': true,
+  'params': '{amendmentId: "d758b0e4-1bcd-43b6-a6aa-99a12f7a402e…}'
+};
+
+const stripes = {
+  'logger': '{categories: "core,action,xhr", prefix: "stripes", …}',
+  'store': '{@@observable: ƒ observable() {}, dispatch: ƒ () {}…}',
+  'epics': '{add: ƒ add() {}, middleware: ƒ epicMiddleware() {}…}',
+  'config': '{hasAllPerms: true}',
+  'okapi': '{bindings: {…}, currentPerms: {…}, currentUser: {…}…}',
+  'withOkapi': true,
+  'setToken': 'ƒ setToken() {}',
+  'actionNames': '["selectPreviousRow", "selectNextRow", "selectFirst…]',
+  'locale': 'en-US',
+  'timezone': 'UTC',
+  'currency': 'USD',
+  'metadata': '{agreements: {…}, dashboard: {…}, eholdings: {…}, e…}',
+  'icons': '{@folio/agreements: {…}, @folio/dashboard: {…}, @fo…}',
+  'setLocale': 'ƒ setLocale() {}',
+  'setTimezone': 'ƒ setTimezone() {}',
+  'setCurrency': 'ƒ setCurrency() {}',
+  'plugins': '{}',
+  'setSinglePlugin': 'ƒ setSinglePlugin() {}',
+  'bindings': '{}',
+  'setBindings': 'ƒ setBindings() {}',
+  'discovery': '{interfaceProviders: Array(68), interfaces: {…}, is…}',
+  'user': '{perms: {…}, user: {…}}',
+  'connect': 'ƒ () {}'
+};
+
+const resources = {
+  'license': '{dataKey: undefined, failed: false, failedMutations…}',
+  'linkedAgreements': '{failed: {…}, failedMutations: Array(0), hasLoaded:…}',
+  'terms': '{dataKey: undefined, failed: false, failedMutations…}'
+};
+
+const okapi = {
+  'url': 'https://folio-snapshot-okapi.dev.folio.org',
+  'tenant': 'diku',
+  'translations': '{stripes-acq-components.FormFooter.cancel: Array(1)…}',
+  'locale': 'en-US',
+  'currentUser': '{addresses: Array(0), curServicePoint: {…}, email: …}',
+  'currentPerms': '{accounts.cancel.post: true, accounts.check-pay.pos…}',
+  'token': 'eyJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJkaWt1X2FkbWluIiwidXNlcl9pZCI6ImVkN2EzMWU1LWJlZDktNTQ3ZS1hNzgwLWRmM2FhYmUwMzYwNyIsInR5cGUiOiJhY2Nlc3MiLCJpYXQiOjE2NDU1MjE0NTgsInRlbmFudCI6ImRpa3UifQ.OiZUeBscZa6S6_dxuaIsMAZIDDQQbV2Gy97cU4sb96s',
+  'plugins': '{}',
+  'bindings': '{}',
+  'ssoEnabled': false,
+  'okapiReady': true
+};
+
+const mutator = {
+  'license': '{DELETE: ƒ DELETE() {}, POST: ƒ POST() {}, PUT: ƒ P…}',
+  'linkedAgreements': '{DELETE: ƒ DELETE() {}, POST: ƒ POST() {}, PUT: ƒ P…}',
+  'terms': '{DELETE: ƒ DELETE() {}, POST: ƒ POST() {}, PUT: ƒ P…}'
+};
+
+export {
+    handlers, history, location, match, stripes, resources, okapi, mutator
+};
