a58dd20e28c1d0f9b4364f47db2760b5f547b5b7
Monireh Rasouli <83644557+MonireRasouli@users.noreply.github.com>
2022-07-18 13:18:30 +0100

    ERM-2251 Add test coverage for AmendmentForm

---
+++ src/components/AmendmentForm/AmendmentForm.test.js
@@ -0,0 +1,109 @@
+import React from 'react';
+import '@folio/stripes-erm-components/test/jest/__mock__';
+import { renderWithIntl, TestForm } from '@folio/stripes-erm-components/test/jest/helpers';
+import { MemoryRouter } from 'react-router-dom';
+import { Button, Pane } from '@folio/stripes-testing';
+import AmendmentForm from './AmendmentForm';
+import {
+  data,
+  handlers,
+  initialValues,
+  isLoading,
+  form,
+  handleSubmit,
+  pristine,
+  submitting,
+  values,
+  mutators,
+  navigationCheck
+} from './testResources';
+import translationsProperties from '../../../test/helpers';
+
+jest.mock('../../hooks', () => ({
+  ...jest.requireActual('../../hooks'),
+  useLicensesContexts: jest.fn(() => ({ data: [] }))
+}));
+
+jest.mock('../formSections/AmendmentFormInfo', () => () => <div>AmendmentFormInfo</div>);
+
+const onSubmit = jest.fn();
+
+describe('AmendmentForm', () => {
+  let renderComponent;
+  beforeEach(() => {
+    renderComponent = renderWithIntl(
+      <MemoryRouter>
+        <TestForm initialValues={initialValues} onSubmit={onSubmit}>
+          <AmendmentForm
+            data={data}
+            form={form}
+            handlers={handlers}
+            handleSubmit={handleSubmit}
+            isLoading={isLoading}
+            mutators={mutators}
+            navigationCheck={navigationCheck}
+            onSubmit={onSubmit}
+            pristine={pristine}
+            submitting={submitting}
+            values={values}
+          />
+        </TestForm>
+      </MemoryRouter>,
+      translationsProperties
+    );
+  });
+
+  test('renders the expected New amendment Pane', async () => {
+    await Pane('New amendment').is({ visible: true });
+  });
+
+  it('renders the AmendmentFormInfo component', () => {
+    const { getByText } = renderComponent;
+    expect(getByText('AmendmentFormInfo')).toBeInTheDocument();
+  });
+
+  test('renders Collapse all button', async () => {
+    await Button('Collapse all').exists();
+  });
+
+  it('renders Core documents accordion', () => {
+    const { getByText } = renderComponent;
+    expect(getByText('Core documents')).toBeInTheDocument();
+  });
+
+  it('renders empty core documents message', () => {
+    const { getByText } = renderComponent;
+    expect(getByText('No core documents have been added.')).toBeInTheDocument();
+  });
+
+  test('renders Add core document button', async () => {
+    await Button('Add core document').exists();
+  });
+
+  it('renders CustomPropertiesEdit component', () => {
+    const { getByText } = renderComponent;
+    expect(getByText('CustomPropertiesEdit')).toBeInTheDocument();
+  });
+
+  it('renders Supplementary documents accordion', () => {
+    const { getByText } = renderComponent;
+    expect(getByText('Supplementary documents')).toBeInTheDocument();
+  });
+
+  it('renders empty supplementary documents message', () => {
+    const { getByText } = renderComponent;
+    expect(getByText('No supplementary documents have been added')).toBeInTheDocument();
+  });
+
+  test('renders Add supplementary document button', async () => {
+    await Button('Add supplementary document').exists();
+  });
+
+  test('renders Cancel button', async () => {
+    await Button('Cancel').exists();
+  });
+
+  test('renders Submit button', async () => {
+    await Button('Submit').exists();
+  });
+});
