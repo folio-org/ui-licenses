e42c97e80df440d8c7cefd79e2ee649f66478463
Monireh Rasouli <83644557+MonireRasouli@users.noreply.github.com>
2022-08-05 09:47:20 +0100

    Add test coverage for Licenses component, ERM-2257 (#584)

---
+++ src/components/Licenses/Licenses.test.js
@@ -0,0 +1,102 @@
+import React from 'react';
+import '@folio/stripes-erm-components/test/jest/__mock__';
+import { renderWithIntl } from '@folio/stripes-erm-components/test/jest/helpers';
+import { MemoryRouter } from 'react-router';
+import { SearchField, MultiColumnList, Pane } from '@folio/stripes-testing';
+import Licenses from './Licenses';
+import {
+  data,
+  history,
+  onCompareLicenseTerms,
+  onNeedMoreData,
+  queryGetter,
+  querySetter,
+  searchString,
+  selectedRecordId,
+  source
+} from './testResources';
+import translationsProperties from '../../../test/helpers';
+
+jest.mock('@folio/stripes-erm-components', () => ({
+  ...jest.requireActual('@folio/stripes-erm-components'),
+  LicenseEndDate: () => <div>LicenseEndDate</div>
+}));
+
+jest.mock('../LicenseFilters', () => () => <div>LicenseFilters</div>);
+
+describe('Licenses', () => {
+  let renderComponent;
+  beforeEach(() => {
+    renderComponent = renderWithIntl(
+      <MemoryRouter>
+        <Licenses
+          data={data}
+          history={history}
+          onCompareLicenseTerms={onCompareLicenseTerms}
+          onNeedMoreData={onNeedMoreData}
+          queryGetter={queryGetter}
+          querySetter={querySetter}
+          searchString={searchString}
+          selectedRecordId={selectedRecordId}
+          source={source}
+        />
+      </MemoryRouter>,
+      translationsProperties
+    );
+  });
+
+  it('renders the LicenseEndDate component', () => {
+    const { getByText } = renderComponent;
+    expect(getByText('LicenseEndDate')).toBeInTheDocument();
+  });
+
+  test('renders expected column headers', async () => {
+    await MultiColumnList({ columns: [' ', 'Name', 'Type', 'Status', 'Start date', 'End date'] }).exists();
+  });
+
+  test('renders the expected Search and filter Pane', async () => {
+    await Pane('Search and filter').is({ visible: true });
+  });
+
+  test('renders the expected Licenses Pane', async () => {
+    await Pane('Licenses').is({ visible: true });
+  });
+
+  test('renders the expected search field', async () => {
+    await SearchField().has({ id: 'input-license-search' });
+  });
+  it('renders the LicenseFilters component', () => {
+    const { getByText } = renderComponent;
+    expect(getByText('LicenseFilters')).toBeInTheDocument();
+  });
+
+  it('renders expected Hide search pane tooltip text', () => {
+    const { getByText } = renderComponent;
+    expect(getByText('Hide search pane')).toBeInTheDocument();
+  });
+
+  test('renders the Hide search pane button', () => {
+    const { getByRole } = renderComponent;
+    expect(getByRole('button', { name: 'Hide search pane' }));
+  });
+
+  test('renders the Search button', () => {
+    const { getByRole } = renderComponent;
+    expect(getByRole('button', { name: 'Search' }));
+  });
+
+  test('renders the Reset all button', () => {
+    const { getByRole } = renderComponent;
+    expect(getByRole('button', { name: 'Reset all' }));
+  });
+
+  test('renders the Hide search pane tooltip', () => {
+    const { getByRole } = renderComponent;
+    expect(getByRole('tooltip', { name: 'Hide search pane' }));
+  });
+
+  test('renders the Search Licenses searchbox', () => {
+    const { getByRole } = renderComponent;
+    expect(getByRole('searchbox', { name: 'Search Licenses' }));
+  });
+});
