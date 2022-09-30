48d4c6eb5b0f690d0287f7323690e5cc2168bfc8
Ethan Freestone <ethan.freestone@k-int.co.uk>
2022-09-30 09:50:22 +0100

    refactor: index.js
    Refactor index.js to functional to allow for hooks
    
    ERM-2316

--- src/index.js
+++ src/index.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import { useState } from 'react';
 import { FormattedMessage } from 'react-intl';
 import PropTypes from 'prop-types';
 import { Switch } from 'react-router-dom';
@@ -31,123 +31,120 @@ import Settings from './settings';
 
 import setUpRegistry from './setUpRegistry';
 
-class App extends React.Component {
-  static eventHandler(event, _s, data) {
-    if (event === 'LOAD_STRIPES_REGISTRY') {
-      // Data should contain Registry singleton:
-      setUpRegistry(data);
-    }
+const App = (props) => {
+  const {
+    actAs,
+    history,
+    location,
+    match: { path },
+    stripes
+  } = props;
 
-    return null;
-  }
+  const [showKeyboardShortcutsModal, setShowKeyboardShortcutsModal] = useState(false);
 
-  static propTypes = {
-    actAs: PropTypes.string.isRequired,
-    history: PropTypes.object,
-    location: PropTypes.object,
-    match: PropTypes.object.isRequired,
-    stripes: PropTypes.object.isRequired,
-  }
-
-  state = {
-    showKeyboardShortcutsModal: false,
-  };
-
-  changeKeyboardShortcutsModal = (modalState) => {
-    this.setState({ showKeyboardShortcutsModal: modalState });
-  };
-
-  shortcutModalToggle(handleToggle) {
+  const shortcutModalToggle = (handleToggle) => {
     handleToggle();
-    this.changeKeyboardShortcutsModal(true);
-  }
+    setShowKeyboardShortcutsModal(true);
+  };
 
-  searchInput = () => {
-    return this.props.location.pathname.search('/licenses') === 0 ?
+  const searchInput = () => {
+    return location.pathname.search('/licenses') === 0 ?
       'input-license-search' :
       undefined;
-  }
+  };
 
-  focusSearchField = () => {
-    const { history, stripes } = this.props;
-    const el = document.getElementById(this.searchInput());
+  const focusSearchField = () => {
+    const el = document.getElementById(searchInput());
     if (el) {
       el.focus();
     } else {
       history.push(stripes.home);
     }
-  }
+  };
 
-  shortcuts = [
+  const shortcuts = [
     {
       name: 'search',
-      handler: this.focusSearchField
+      handler: focusSearchField
     },
     {
       name: 'openShortcutModal',
-      handler: this.changeKeyboardShortcutsModal
+      handler: setShowKeyboardShortcutsModal
     },
   ];
 
-  render() {
-    const { actAs, match: { path } } = this.props;
-
-    if (actAs === 'settings') {
-      return (
-        <Settings {...this.props} />
-      );
-    }
-
+  if (actAs === 'settings') {
     return (
-      <>
-        <CommandList commands={defaultKeyboardShortcuts}>
-          <HasCommand
-            commands={this.shortcuts}
-            isWithinScope={checkScope}
-            scope={document.body}
-          >
-            <AppContextMenu>
-              {(handleToggle) => (
-                <NavList>
-                  <NavListSection>
-                    <NavListItem
-                      id="keyboard-shortcuts-item"
-                      onClick={() => { this.shortcutModalToggle(handleToggle); }}
-                    >
-                      <FormattedMessage id="ui-licenses.appMenu.keyboardShortcuts" />
-                    </NavListItem>
-                  </NavListSection>
-                </NavList>
-              )}
-            </AppContextMenu>
-            <Switch>
-              <Route component={NoteCreateRoute} exact path={`${path}/notes/create`} />
-              <Route component={NoteViewRoute} exact path={`${path}/notes/:noteId`} />
-              <Route component={NoteEditRoute} exact path={`${path}/notes/:noteId/edit`} />
-              <Route component={CreateLicenseRoute} path={`${path}/create`} />
-              <Route component={EditLicenseRoute} path={`${path}/:id/edit`} />
-              <Route component={CreateAmendmentRoute} path={`${path}/:id/amendments/create`} />
-              <Route component={EditAmendmentRoute} path={`${path}/:id/amendments/:amendmentId/edit`} />
-              <Route component={LicensesRoute} path={`${path}/:id?`}>
-                <Switch>
-                  <Route exact path={`${path}/:id`} render={(props) => (<ViewLicenseRoute key={props.match.params.id} {...props} />)} />
-                  <Route component={ViewAmendmentRoute} exact path={`${path}/:id/amendments/:amendmentId`} />
-                </Switch>
-              </Route>
-            </Switch>
-          </HasCommand>
-        </CommandList>
-        {this.state.showKeyboardShortcutsModal && (
-          <KeyboardShortcutsModal
-            allCommands={defaultKeyboardShortcuts}
-            onClose={() => { this.changeKeyboardShortcutsModal(false); }}
-            open
-          />
-        )}
-      </>
+      <Settings {...props} />
     );
   }
-}
+
+  return (
+    <>
+      <CommandList commands={defaultKeyboardShortcuts}>
+        <HasCommand
+          commands={shortcuts}
+          isWithinScope={checkScope}
+          scope={document.body}
+        >
+          <AppContextMenu>
+            {(handleToggle) => (
+              <NavList>
+                <NavListSection>
+                  <NavListItem
+                    id="keyboard-shortcuts-item"
+                    onClick={() => { shortcutModalToggle(handleToggle); }}
+                  >
+                    <FormattedMessage id="ui-licenses.appMenu.keyboardShortcuts" />
+                  </NavListItem>
+                </NavListSection>
+              </NavList>
+            )}
+          </AppContextMenu>
+          <Switch>
+            <Route component={NoteCreateRoute} exact path={`${path}/notes/create`} />
+            <Route component={NoteViewRoute} exact path={`${path}/notes/:noteId`} />
+            <Route component={NoteEditRoute} exact path={`${path}/notes/:noteId/edit`} />
+            <Route component={CreateLicenseRoute} path={`${path}/create`} />
+            <Route component={EditLicenseRoute} path={`${path}/:id/edit`} />
+            <Route component={CreateAmendmentRoute} path={`${path}/:id/amendments/create`} />
+            <Route component={EditAmendmentRoute} path={`${path}/:id/amendments/:amendmentId/edit`} />
+            <Route component={LicensesRoute} path={`${path}/:id?`}>
+              <Switch>
+                <Route exact path={`${path}/:id`} render={(routeProps) => (<ViewLicenseRoute key={routeProps.match.params.id} {...routeProps} />)} />
+                <Route component={ViewAmendmentRoute} exact path={`${path}/:id/amendments/:amendmentId`} />
+              </Switch>
+            </Route>
+          </Switch>
+        </HasCommand>
+      </CommandList>
+      {showKeyboardShortcutsModal && (
+        <KeyboardShortcutsModal
+          allCommands={defaultKeyboardShortcuts}
+          onClose={() => { setShowKeyboardShortcutsModal(false); }}
+          open
+        />
+      )}
+    </>
+  );
+};
+
+App.eventHandler = (event, _s, data) => {
+  if (event === 'LOAD_STRIPES_REGISTRY') {
+    // Data should contain Registry singleton:
+    setUpRegistry(data);
+  }
+
+  return null;
+};
+
+App.propTypes = {
+  actAs: PropTypes.string.isRequired,
+  history: PropTypes.object,
+  location: PropTypes.object,
+  match: PropTypes.object.isRequired,
+  stripes: PropTypes.object.isRequired,
+};
 
 export default App;
 export { default as Licenses } from './components/Licenses';
