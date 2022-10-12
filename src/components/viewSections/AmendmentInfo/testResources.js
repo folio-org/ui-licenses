ae4aaf50375c0839b52ae7526a71ad0ecaaf885b
Monireh Rasouli <83644557+MonireRasouli@users.noreply.github.com>
2022-10-12 10:52:08 +0100

    ERM-2260 , Add test coverage for AmendmentInfo (#608)

---
+++ src/components/viewSections/AmendmentInfo/testResources.js
@@ -0,0 +1,38 @@
+const amendment = {
+  'id': 'f35483c9-98e5-43eb-b5ba-ad5ef409ee6e',
+  'endDateSemantics': {
+    'id': '2c91809d83c4b9180183c4c05c210001',
+    'value': 'explicit',
+    'label': 'Explicit',
+    'owner': {
+      'id': '2c91809d83c4b9180183c4c05bc90000',
+      'desc': 'License.EndDateSemantics',
+      'internal': true
+    }
+  },
+  'dateCreated': '2022-10-11T12:47:51Z',
+  'customProperties': '{}',
+  'contacts': '[]',
+  'tags': '[]',
+  'lastUpdated': '2022-10-11T12:52:50Z',
+  'docs': '[]',
+  'name': 'MR test amendment',
+  'status': {
+    'id': '2c91809d83c4b9180183c4c05c420007',
+    'value': 'active',
+    'label': 'Active',
+    'owner': {
+      'id': '2c91809d83c4b9180183c4c05c340004',
+      'desc': 'License.Status',
+      'internal': true
+    }
+  },
+  'supplementaryDocs': '[]',
+  'description': 'MR test amendment description.',
+  'startDate': '2022-10-01',
+  'endDate': '2022-10-05',
+  'openEnded': false
+};
+
+
+export default amendment;
