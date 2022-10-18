5d69eeef76d1bfa4dbd5592778938200a7e8d53c
Monireh Rasouli <83644557+MonireRasouli@users.noreply.github.com>
2022-10-18 12:26:13 +0100

    ERM-2263, Add test coverage for LicenseAgreements

---
+++ src/components/viewSections/LicenseAgreements/testResources.js
@@ -0,0 +1,207 @@
+const amendment = {
+  'id': 'a73b477d-63e6-46c1-baa8-7de7f907f6a8',
+  'endDateSemantics': {
+    'id': '2c91809d83e39e9d0183e3a5b9680003',
+    'value': 'implicit',
+    'label': 'Implicit',
+    'owner': {
+      'id': '2c91809d83e39e9d0183e3a5b9160000',
+      'desc': 'License.EndDateSemantics',
+      'internal': true
+    }
+  },
+  'dateCreated': '2022-10-17T12:04:57Z',
+  'customProperties': '{}',
+  'contacts': '[]',
+  'tags': '[]',
+  'lastUpdated': '2022-10-17T12:04:57Z',
+  'docs': '[]',
+  'name': 'MR Amendment',
+  'status': '{id: "2c91809d83e39e9d0183e3a5b9790007", label: "Ac…}',
+  'supplementaryDocs': '[]',
+  'openEnded': false
+};
+
+const id = 'licenseAgreements';
+
+const license = {
+  'id': 'fd842fde-a101-40c0-b657-f87e2ac5a510',
+  'endDateSemantics': {
+    'id': '2c91809d83e39e9d0183e3a5b95b0001',
+    'value': 'explicit',
+    'label': 'Explicit',
+    'owner': {
+      'id': '2c91809d83e39e9d0183e3a5b9160000',
+      'desc': 'License.EndDateSemantics',
+      'internal': true
+    }
+  },
+  'dateCreated': '2022-10-17T11:19:33Z',
+  'customProperties': '{}',
+  'contacts': '[]',
+  'tags': '[]',
+  'lastUpdated': '2022-10-17T12:04:57Z',
+  'docs': '[]',
+  'name': 'MR Test License 1',
+  'status': {
+    'id': '2c91809d83e39e9d0183e3a5b9790007',
+    'value': 'active',
+    'label': 'Active',
+    'owner': {
+      'id': '2c91809d83e39e9d0183e3a5b96d0004',
+      'desc': 'License.Status',
+      'internal': true
+    }
+  },
+  'supplementaryDocs': '[]',
+  'description': 'This is a description of MR Test License 1.',
+  'startDate': '2022-10-01',
+  'endDate': '2022-10-31',
+  'openEnded': false,
+  'amendments': [{
+    'id': 'a73b477d-63e6-46c1-baa8-7de7f907f6a8',
+    'endDateSemantics': {
+      'id': '2c91809d83e39e9d0183e3a5b9680003',
+      'value': 'implicit',
+      'label': 'Implicit',
+      'owner': {
+        'id': '2c91809d83e39e9d0183e3a5b9160000',
+        'desc': 'License.EndDateSemantics',
+        'internal': true
+      }
+    },
+    'dateCreated': '2022-10-17T12:04:57Z',
+    'customProperties': '{}',
+    'contacts': '[]',
+    'tags': '[]',
+    'lastUpdated': '2022-10-17T12:04:57Z',
+    'docs': '[]',
+    'name': 'MR Amendment',
+    'status': '{id: "2c91809d83e39e9d0183e3a5b9790007", label: "Ac…}',
+    'supplementaryDocs': '[]',
+    'openEnded': false
+  }],
+  'orgs': '[]',
+  'type': {
+    'id': '2c91809d83e39e9d0183e3a5b98a000b',
+    'value': 'local',
+    'label': 'Local',
+    'owner': {
+      'id': '2c91809d83e39e9d0183e3a5b988000a',
+      'desc': 'License.Type',
+      'internal': false
+    }
+  },
+  'alternateNames': '[]',
+  'linkedAgreements': [{
+    'id': '2c91809c83e657ed0183ea32bf6e0082',
+    'remoteId': '71ee43f8-3860-4f20-b35d-2d6c786f576e',
+    'owner': {
+      'id': '4792fad8-a2d7-4a08-9516-3d519bcbfa2e',
+      'dateCreated': '2022-10-18T08:24:36Z',
+      'name': 'MR Test Agreement',
+      'orgs': [],
+      'externalLicenseDocs': [],
+      'outwardRelationships': [],
+      'customProperties': {},
+      'contacts': [],
+      'tags': [],
+      'lastUpdated': '2022-10-18T08:25:37Z',
+      'inwardRelationships': [],
+      'endDate': '2022-10-31',
+      'startDate': '2022-10-01',
+      'docs': [],
+      'periods': [
+        '{endDate: "2022-10-31", id: "95800f9b-abd6-4faf-a23…}'
+      ],
+      'usageDataProviders': [],
+      'agreementStatus': {
+        'id': '2c91809c83e657ed0183e65f54510016',
+        'value': 'active',
+        'label': 'Active'
+      },
+      'supplementaryDocs': [],
+      'description': 'This is a description of MR Test Agreement.',
+      'cancellationDeadline': null,
+      'alternateNames': [],
+      'version': 1
+    },
+    'amendments': [],
+    'status': {
+      'id': '2c91809c83e657ed0183e65f54590018',
+      'value': 'controlling',
+      'label': 'Controlling'
+    },
+    'note': 'test'
+  },
+  {
+    'id': '2c91809c83e657ed0183ea7d156b0083',
+    'remoteId': '71ee43f8-3860-4f20-b35d-2d6c786f576e',
+    'owner': {
+      'id': 'cd338736-3bd3-4ad1-9178-317fa555574a',
+      'dateCreated': '2022-10-18T09:46:21Z',
+      'name': 'test',
+      'orgs': [],
+      'externalLicenseDocs': [],
+      'outwardRelationships': [],
+      'customProperties': {},
+      'contacts': [],
+      'tags': [],
+      'lastUpdated': '2022-10-18T09:46:49Z',
+      'inwardRelationships': [],
+      'startDate': '2022-10-07',
+      'docs': [],
+      'periods': [
+        '{id: "f1a425d1-5120-4d26-9fe2-84f78d8ea921", owner:…}'
+      ],
+      'usageDataProviders': [],
+      'agreementStatus': {
+        'id': '2c91809c83e657ed0183e65f54510016',
+        'value': 'active',
+        'label': 'Active'
+      },
+      'supplementaryDocs': [],
+      'cancellationDeadline': null,
+      'alternateNames': [],
+      'version': 1
+    },
+    'amendments': [{
+      'id': '1a356524-3a98-4e8b-bdff-82000fba49a6',
+      'owner': {
+        'id': '2c91809c83e657ed0183ea7d156b0083'
+      },
+      'status': {
+        'id': '2c91809c83e657ed0183e65f53df0001',
+        'value': 'current',
+        'label': 'Current'
+      },
+      'amendmentId': 'bd046c7c-2631-4dfd-adef-69b69baa5d07',
+      'note': 'test'
+    }],
+    'status': {
+      'id': '2c91809c83e657ed0183e65f54590018',
+      'value': 'controlling',
+      'label': 'Controlling'
+    },
+    'note': 'test'
+  }
+  ]
+};
+
+
+const visibleColumns = [
+  'linkNote',
+  'name',
+  'startDate',
+  'endDate',
+  'agreementStatus',
+  'linkStatus',
+  'amendmentLinkStatus'
+];
+
+export {
+  amendment,
+  id,
+  license,
+  visibleColumns
+};
