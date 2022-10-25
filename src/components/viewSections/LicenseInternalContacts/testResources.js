b26de6a6743fe502042d87561b13348943c72333
Monireh Rasouli <83644557+MonireRasouli@users.noreply.github.com>
2022-10-25 10:21:20 +0100

    ERM-2267- Add test coverage for LicenseInternalContacts (#619)

---
+++ src/components/viewSections/LicenseInternalContacts/testResources.js
@@ -0,0 +1,94 @@
+const license = {
+  'id': '227ec182-a85f-4d71-89ca-1e87a6444f18',
+  'dateCreated': '2022-10-20T08:39:00Z',
+  'customProperties': '{}',
+  'contacts': [{
+    'id': '2c91809d83f3138c0183f49139260047',
+    'owner': {
+      'id': '227ec182-a85f-4d71-89ca-1e87a6444f18'
+    },
+    'role': {
+      'id': '2c91809d83f3138c0183f31a81870002',
+      'value': 'erm_librarian',
+      'label': 'ERM Librarian',
+      'owner': {
+        'id': '2c91809d83f3138c0183f31a81180000',
+        'desc': 'InternalContact.Role',
+        'internal': false
+      }
+    },
+    'user': {
+      'username': 'josefa',
+      'id': '384272bb-efab-4e94-b3b8-f67f20796f20',
+      'barcode': '217874037910807',
+      'active': true,
+      'type': 'patron',
+      'patronGroup': 'ad0bc554-d5bc-463c-85d1-5562127ae91b',
+      'departments': [],
+      'proxyFor': [],
+      'personal': {
+        'lastName': 'Block',
+        'firstName': 'Araceli',
+        'middleName': 'Avery',
+        'email': 'fredy@lehner-and-sons.sr',
+        'phone': '890-719-9790 x316',
+        'dateOfBirth': '1988-05-07T00:00:00.000+00:00',
+        'addresses': [{
+          'countryId': 'US',
+          'addressLine1': '67186 Wendy Squares',
+          'city': 'Fort Smith',
+          'region': 'MI',
+          'postalCode': '02901',
+          'addressTypeId': '93d3d88d-499b-45d0-9bc7-ac73c3a19880',
+          'primaryAddress': true
+        }],
+        'preferredContactTypeId': '002'
+      },
+      'enrollmentDate': '2015-06-25T00:00:00.000+00:00',
+      'expirationDate': '2022-11-30T23:59:59.000+00:00',
+      'createdDate': '2022-10-20T06:42:11.976+00:00',
+      'updatedDate': '2022-10-20T06:42:11.976+00:00',
+      'metadata': {
+        'createdDate': '2022-10-20T01:54:37.430+00:00',
+        'updatedDate': '2022-10-20T06:42:11.973+00:00',
+        'updatedByUserId': '28645afd-c318-5410-8552-c1e3d357e6ab'
+      },
+      'customFields': {}
+    }
+  }],
+  'tags': '[]',
+  'lastUpdated': '2022-10-20T08:45:33Z',
+  'docs': '[]',
+  'name': 'MR Test License',
+  'status': {
+    'id': '2c91809d83f3138c0183f31a81ca000f',
+    'value': 'active',
+    'label': 'Active',
+    'owner': {
+      'id': '2c91809d83f3138c0183f31a81be000c',
+      'desc': 'License.Status',
+      'internal': true
+    }
+  },
+  'supplementaryDocs': '[]',
+  'description': 'test',
+  'startDate': '2022-10-01',
+  'endDate': '2022-10-31',
+  'openEnded': false,
+  'amendments': '[]',
+  'orgs': '[]',
+  'type': {
+    'id': '2c91809d83f3138c0183f31a81ef0017',
+    'value': 'local',
+    'label': 'Local',
+    'owner': {
+      'id': '2c91809d83f3138c0183f31a81ed0016',
+      'desc': 'License.Type',
+      'internal': false
+    }
+  },
+  'alternateNames': '[]',
+  'linkedAgreements': '[]'
+};
+
+export default license;
