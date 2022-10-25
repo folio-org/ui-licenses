8b6f11d1f48cd42d79934308bb2694e6253765f2
Monireh Rasouli <83644557+MonireRasouli@users.noreply.github.com>
2022-10-25 10:24:36 +0100

    ERM-2270, Add test coverage for CreateLicenseRoute (#620)

---
+++ src/routes/CreateLicenseRoute/CreateLicenseRoute.test.js
@@ -0,0 +1,65 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import '@folio/stripes-erm-components/test/jest/__mock__';
+import { renderWithIntl } from '@folio/stripes-erm-components/test/jest/helpers';
+import { MemoryRouter } from 'react-router-dom';
+import { Button } from '@folio/stripes/components';
+import { Button as ButtonInteractor } from '@folio/stripes-testing';
+import translationsProperties from '../../../test/helpers';
+import CreateLicenseRoute from './CreateLicenseRoute';
+
+const CloseButton = (props) => {
+  return <Button onClick={props.handlers.onClose}>CloseButton</Button>;
+};
+
+CloseButton.propTypes = {
+  handlers: PropTypes.shape({
+    onClose: PropTypes.func,
+  }),
+};
+const historyPushMock = jest.fn();
+
+jest.mock('../../components/LicenseForm', () => {
+  return (props) => (
+    <div>
+      <div>LicenseForm</div>
+      <CloseButton {...props} />
+    </div>
+  );
+});
+
+const data = {
+  history: {
+    push: historyPushMock
+  },
+  location: {
+    search: ''
+  }
+};
+
+describe('CreateLicenseRoute', () => {
+  let renderComponent;
+  beforeEach(() => {
+    renderComponent = renderWithIntl(
+      <MemoryRouter>
+        <CreateLicenseRoute {...data} />
+      </MemoryRouter>,
+      translationsProperties
+    );
+  });
+
+  test('renders the LicenseForm component', () => {
+    const { getByText } = renderComponent;
+    expect(getByText('LicenseForm')).toBeInTheDocument();
+  });
+
+  test('renders the CloseButton ', () => {
+    const { getByText } = renderComponent;
+    expect(getByText('CloseButton')).toBeInTheDocument();
+  });
+
+  test('triggers the CloseButton callback', async () => {
+    await ButtonInteractor('CloseButton').click();
+    expect(historyPushMock).toHaveBeenCalled();
+  });
+});
