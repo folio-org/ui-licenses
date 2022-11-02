790ba59870f5b0b6c8523515afd1bc166f021915
Peter <boehm@hebis.uni-frankfurt.de>
2022-11-02 12:40:31 +0100

    ERM-2261

---
+++ src/components/viewSections/AmendmentLicense/testResources.js
@@ -0,0 +1,269 @@
+const urls = {
+  licenseView: id => `/licenses/${id}`,
+}
+
+const license = {
+  "id": "56319d8b-c5f1-4b40-9fa3-78aca5c5f311",
+  "endDateSemantics": {
+    "id": "2c91809d843605f00184360ea4900012",
+    "value": "explicit",
+    "label": "Explicit",
+    "owner": {
+      "id": "2c91809d843605f00184360ea48b0011",
+      "desc": "License.EndDateSemantics",
+      "internal": true
+    }
+  },
+  "dateCreated": "2022-11-02T10:23:28Z",
+  "customProperties": {},
+  "contacts": [],
+  "tags": [],
+  "lastUpdated": "2022-11-02T11:21:56Z",
+  "docs": [],
+  "name": "TestLicense",
+  "status": {
+    "id": "2c91809d843605f00184360ea4c20018",
+    "value": "active",
+    "label": "Active",
+    "owner": {
+      "id": "2c91809d843605f00184360ea4ad0015",
+      "desc": "License.Status",
+      "internal": true
+    }
+  },
+  "supplementaryDocs": [],
+  "startDate": "2022-06-01",
+  "endDate": "2022-11-30",
+  "openEnded": false,
+  "amendments": [
+    {
+      "id": "abf859a7-f20d-4abc-b805-98dfea112513",
+      "endDateSemantics": {
+        "id": "2c91809d843605f00184360ea4a50014",
+        "value": "implicit",
+        "label": "Implicit",
+        "owner": {
+          "id": "2c91809d843605f00184360ea48b0011",
+          "desc": "License.EndDateSemantics",
+          "internal": true
+        }
+      },
+      "dateCreated": "2022-11-02T10:23:37Z",
+      "customProperties": {},
+      "contacts": [],
+      "tags": [],
+      "lastUpdated": "2022-11-02T11:21:56Z",
+      "docs": [],
+      "name": "A1",
+      "status": {
+        "id": "2c91809d843605f00184360ea4c20018",
+        "value": "active",
+        "label": "Active",
+        "owner": {
+          "id": "2c91809d843605f00184360ea4ad0015",
+          "desc": "License.Status",
+          "internal": true
+        }
+      },
+      "supplementaryDocs": [],
+      "openEnded": false
+    },
+    {
+      "id": "8d0785a4-580d-4456-96c0-cceda72b6f54",
+      "endDateSemantics": {
+        "id": "2c91809d843605f00184360ea4a50014",
+        "value": "implicit",
+        "label": "Implicit",
+        "owner": {
+          "id": "2c91809d843605f00184360ea48b0011",
+          "desc": "License.EndDateSemantics",
+          "internal": true
+        }
+      },
+      "dateCreated": "2022-11-02T10:23:49Z",
+      "customProperties": {},
+      "contacts": [],
+      "tags": [],
+      "lastUpdated": "2022-11-02T11:21:56Z",
+      "docs": [],
+      "name": "A2",
+      "status": {
+        "id": "2c91809d843605f00184360ea4c20018",
+        "value": "active",
+        "label": "Active",
+        "owner": {
+          "id": "2c91809d843605f00184360ea4ad0015",
+          "desc": "License.Status",
+          "internal": true
+        }
+      },
+      "supplementaryDocs": [],
+      "openEnded": false
+    }
+  ],
+  "orgs": [
+    {
+      "id": "8f4bd69e-da33-4aed-aa09-35c5f5b14ec1",
+      "primaryOrg": true,
+      "org": {
+        "id": "a1a97de2-db37-4bef-aeaa-45891c7dd560",
+        "orgsUuid": "11fb627a-cdf1-11e8-a8d5-f2801f1b9fd1",
+        "name": "Alexander Street Press",
+        "orgsUuid_object": {
+          "id": "11fb627a-cdf1-11e8-a8d5-f2801f1b9fd1",
+          "name": "Alexander Street Press",
+          "code": "ALEXS",
+          "description": "AV streaming services",
+          "exportToAccounting": false,
+          "status": "Active",
+          "organizationTypes": [],
+          "language": "en-us",
+          "aliases": [
+            {
+              "value": "Alexander Street",
+              "description": ""
+            }
+          ],
+          "addresses": [
+            {
+              "addressLine1": "3212 Duke Street",
+              "addressLine2": "",
+              "city": "Alexandria",
+              "stateRegion": "VA",
+              "zipCode": "22314",
+              "country": "USA",
+              "isPrimary": true,
+              "categories": [],
+              "language": "English"
+            }
+          ],
+          "phoneNumbers": [
+            {
+              "phoneNumber": "1-800-889-5937",
+              "categories": [],
+              "isPrimary": true,
+              "language": "English"
+            }
+          ],
+          "emails": [
+            {
+              "value": "customerservice@alexanderstreet.com",
+              "description": "main customer service email",
+              "isPrimary": true,
+              "categories": [],
+              "language": "English"
+            }
+          ],
+          "urls": [
+            {
+              "value": "https://alexanderstreet.com/",
+              "description": "main website",
+              "language": "en-us",
+              "isPrimary": true,
+              "categories": [
+                "f52ceea4-8e35-404b-9ebd-5c7db6613195"
+              ],
+              "notes": ""
+            }
+          ],
+          "contacts": [
+            "11fb627a-cdf1-11e8-a8d5-f2801f1b9fd1"
+          ],
+          "agreements": [
+            {
+              "name": "library access",
+              "discount": 0,
+              "referenceUrl": "",
+              "notes": ""
+            }
+          ],
+          "erpCode": "G64758-74828",
+          "paymentMethod": "Physical Check",
+          "accessProvider": true,
+          "governmental": false,
+          "licensor": true,
+          "materialSupplier": true,
+          "vendorCurrencies": [
+            "USD"
+          ],
+          "claimingInterval": 0,
+          "discountPercent": 0,
+          "expectedActivationInterval": 1,
+          "expectedInvoiceInterval": 30,
+          "renewalActivationInterval": 365,
+          "subscriptionInterval": 365,
+          "expectedReceiptInterval": 30,
+          "taxId": "",
+          "liableForVat": false,
+          "taxPercentage": 0,
+          "interfaces": [
+            "14e81009-0f98-45a0-b8e6-e25547beb22f"
+          ],
+          "accounts": [
+            {
+              "name": "Library account",
+              "accountNo": "1234",
+              "description": "Main library account",
+              "appSystemNo": "",
+              "paymentMethod": "Physical Check",
+              "accountStatus": "Active",
+              "contactInfo": "customerservice@alexanderstreet.com",
+              "libraryCode": "COB",
+              "libraryEdiCode": "765987610",
+              "notes": "",
+              "acqUnitIds": []
+            }
+          ],
+          "isVendor": true,
+          "sanCode": "1234567",
+          "changelogs": [
+            {
+              "description": "This is a sample note.",
+              "timestamp": "2008-05-15T10:53:00.000+00:00"
+            }
+          ],
+          "acqUnitIds": [],
+          "metadata": {
+            "createdDate": "2022-11-02T01:56:35.508+00:00",
+            "updatedDate": "2022-11-02T01:56:35.508+00:00"
+          }
+        }
+      },
+      "owner": {
+        "id": "56319d8b-c5f1-4b40-9fa3-78aca5c5f311"
+      },
+      "roles": [
+        {
+          "id": "d86ba5e3-7c40-4533-a52f-d7dc060e737a",
+          "owner": {
+            "id": "8f4bd69e-da33-4aed-aa09-35c5f5b14ec1"
+          },
+          "role": {
+            "id": "2c91809d843605f00184360ea45e000b",
+            "value": "licensor",
+            "label": "Licensor",
+            "owner": {
+              "id": "2c91809d843605f00184360ea45a000a",
+              "desc": "LicenseOrg.Role",
+              "internal": false
+            }
+          }
+        }
+      ]
+    }
+  ],
+  "type": {
+    "id": "2c91809d843605f00184360ea469000d",
+    "value": "local",
+    "label": "Local",
+    "owner": {
+      "id": "2c91809d843605f00184360ea465000c",
+      "desc": "License.Type",
+      "internal": false
+    }
+  },
+  "alternateNames": []
+};
+
+
+export {license, urls};
