e4aa525325d8168c20ab62947d938e3cb0351da9
Peter <boehm@hebis.uni-frankfurt.de>
2022-11-09 17:50:38 +0100

    ERM-2261: Add test coverage for ui-licenses viewSections <AmendmentLicense> (#637)
    * ERM-2261
    
    * ERM-2261
    
    * ERM-2261
    
    * ERM-2261: prepare for changes from ERM-2420

---
+++ src/components/viewSections/AmendmentLicense/AmendmentLicense.test.js
@@ -0,0 +1,46 @@
+import React from 'react';
+import '@folio/stripes-erm-components/test/jest/__mock__';
+import { renderWithIntl } from '@folio/stripes-erm-components/test/jest/helpers';
+import { KeyValue } from '@folio/stripes-testing';
+import { MemoryRouter } from 'react-router-dom';
+import translationsProperties from '../../../../test/helpers';
+import { license, urls } from './testResources';
+import AmendmentLicense from './AmendmentLicense';
+
+describe('AmendmentLicense', () => {
+  let renderComponent;
+  beforeEach(() => {
+    renderComponent = renderWithIntl(
+      <MemoryRouter>
+        <AmendmentLicense license={license} urls={urls} />
+      </MemoryRouter>,
+      translationsProperties
+    );
+  });
+
+  test('renders the expected parent license', () => {
+    const { getByText } = renderComponent;
+    expect(getByText(license.name)).toBeInTheDocument();
+  });
+
+  test('renders links with the parent license names', () => {
+    const { getByRole } = renderComponent;
+    expect(getByRole('link', { name: 'TestLicense' })).toBeInTheDocument();
+  });
+
+  test('renders the expected License status', async () => {
+    await KeyValue('Status').has({ value: 'Active' });
+  });
+
+  test('renders the expected License start date value', async () => {
+    await KeyValue('Start date').has({ value: '6/1/2022' });
+  });
+
+  test('renders the expected License end date value', async () => {
+    await KeyValue('End date').has({ value: '11/30/2022' });
+  });
+
+  test('renders the expected Primary org value', async () => {
+    await KeyValue('Primary organization').has({ value: 'Alexander Street Press' });
+  });
+});
