8eee5055ecdf34e2fe354b15718063f32bfbd82d
EthanFreestone <54310740+EthanFreestone@users.noreply.github.com>
2023-02-21 10:57:16 +0000

    chore: Pre release linting (#660)
    Small tweaks to help console errors

--- src/routes/ViewAmendmentRoute/ViewAmendmentRoute.js
+++ src/routes/ViewAmendmentRoute/ViewAmendmentRoute.js
@@ -29,10 +29,6 @@ const propTypes = {
       id: PropTypes.string.isRequired,
     }).isRequired,
   }).isRequired,
-  stripes: PropTypes.shape({
-    hasPerm: PropTypes.func.isRequired,
-    okapi: PropTypes.object.isRequired,
-  }).isRequired,
 };
 
 const ViewAmendmentRoute = ({
