b955af6e182257a60d7fb1629db6b8f22ecdc85a
Jack Golding <jack.golding@k-int.com>
2023-11-27 06:19:06 +0000

    test: Updated tests
    Updated tests to involve functionality

--- src/components/ContentFilter/ContentFilter.test.js
+++ src/components/ContentFilter/ContentFilter.test.js
@@ -4,9 +4,10 @@ import {
   Dropdown,
   renderWithIntl,
 } from '@folio/stripes-erm-testing';
+import { waitFor } from '@folio/jest-config-stripes/testing-library/react';
 import { MemoryRouter } from 'react-router-dom';
 import translationsProperties from '../../../test/helpers';
-import { activeFilters } from './testResources';
+import { activeFilters, data } from './testResources';
 import ContentFilter from './ContentFilter';
 
 const stateMock = jest.fn();
@@ -18,15 +19,11 @@ const filterHandlers = {
   clearGroup: () => {},
   reset: () => {},
 };
-describe('ContentFilter', () => {
+describe('ContentFilter without active filters', () => {
   beforeEach(() => {
     renderWithIntl(
       <MemoryRouter>
-        <ContentFilter
-          activeFilters={activeFilters}
-          filterHandlers={filterHandlers}
-          name="contentFilters"
-        />
+        <ContentFilter filterHandlers={filterHandlers} name="contentFilters" />
       </MemoryRouter>,
       translationsProperties
     );
@@ -44,5 +41,48 @@ describe('ContentFilter', () => {
 
   test('renders the And/Or dropdown', async () => {
     await Dropdown('Add filter').exists();
+  });
+});
+
+describe('ContentFilter with active filters', () => {
+  beforeEach(() => {
+    renderWithIntl(
+      <MemoryRouter>
+        <ContentFilter
+          activeFilters={activeFilters}
+          contentOptions={data.contentOptions}
+          filterHandlers={filterHandlers}
+          name="contentFilters"
+        />
+      </MemoryRouter>,
+      translationsProperties
+    );
+  });
+
+  test('renders the Has/Has not fields with expected values', async () => {
+    await Select({ id: 'contentFilters[0]-attribute-select' }).has({
+      value: ' isNotEmpty',
+    });
+    await Select({ id: 'contentFilters[1]-attribute-select' }).has({
+      value: ' isEmpty',
+    });
+  });
+
+  test('renders the Content fields with expected values', async () => {
+    await MultiSelect({
+      id: 'contentFilters[0]-content-multi-select',
+    }).has({ selected: ['Amendments'] });
+    await MultiSelect({
+      id: 'contentFilters[1]-content-multi-select',
+    }).has({ selected: ['Core documents'] });
+  });
+
+  test('changing the value within the attribute select invokes the callback', async () => {
+    await waitFor(async () => {
+      await Select({ id: 'contentFilters[0]-attribute-select' }).choose(
+        'Has not'
+      );
+    });
+    expect(filterHandlers.state.mock.calls.length).toBe(1);
   });
 });
