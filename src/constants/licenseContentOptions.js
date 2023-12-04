de2a69622d5f253383057027767c36b4d4361c13
Jack-Golding <94838251+Jack-Golding@users.noreply.github.com>
2023-12-04 10:17:28 +0100

    ERM-3062: Add has/has not filter in Licenses and amendments (#702)
    * feat: Has/Has not filters
    
    Added has/has not filter in Licenses and amendments
    
    ERM-3062
    
    * test: Updated tests
    
    Updated tests to involve functionality

---
+++ src/constants/licenseContentOptions.js
@@ -0,0 +1,30 @@
+export default [
+  {
+    value: 'alternateNames',
+    id: 'ui-licenses.alternativeNames',
+  },
+  {
+    value: 'contacts',
+    id: 'ui-licenses.section.internalContacts',
+  },
+  {
+    value: 'orgs',
+    id: 'ui-licenses.filters.organization',
+  },
+  {
+    value: 'docs',
+    id: 'ui-licenses.duplicateAmendmentModal.coreDocs',
+  },
+  {
+    value: 'supplementaryDocs',
+    id: 'ui-licenses.duplicateLicenseModal.supplementaryDocs',
+  },
+  {
+    value: 'amendments',
+    id: 'ui-licenses.amendments',
+  },
+  {
+    value: 'tags',
+    id: 'ui-licenses.tags',
+  },
+];
