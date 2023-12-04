0d8c53e9a2a2e0b7d6e3ae809df636206508a4ab
Claudia Malzer <42971992+CalamityC@users.noreply.github.com>
2023-12-04 17:40:13 +0100

    ERM-1910 Swap Licenses Pick lists settings screen to single 4 pane ba… (#704)
    * ERM-1910 Swap Licenses Pick lists settings screen to single 4 pane based layout
    
    * render RefdataCategoriesSettings instead of EditableRefdataCategoryList in PickListSettings
    
    * ERM-1910 Swap Licenses Pick lists settings screen to single 4 pane based layout
    
    * fix test
    
    * ERM-1910 Swap Licenses Pick lists settings screen to single 4 pane based layout
    
    * change stripes-kint-components dependency to 5.3.0

--- src/settings/routes/PickListSettings.js
+++ src/settings/routes/PickListSettings.js
@@ -1,10 +1,8 @@
 import { useHistory } from 'react-router';
-import { FormattedMessage } from 'react-intl';
 
-import { EditableRefdataCategoryList } from '@k-int/stripes-kint-components';
+import { RefdataCategoriesSettings } from '@k-int/stripes-kint-components';
 
 import { useStripes } from '@folio/stripes/core';
-import { Pane } from '@folio/stripes/components';
 
 import { REFDATA_ENDPOINT } from '../../constants';
 
@@ -20,23 +18,11 @@ const PickListSettings = () => {
   };
 
   return (
-    <Pane
-      defaultWidth="fill"
-      dismissible
-      id="edit-refdata-desc"
+    <RefdataCategoriesSettings
+      displayConditions={displayConditions}
       onClose={() => history.push('/settings/licenses')}
-      paneTitle={
-        <FormattedMessage id="ui-licenses.settings.pickLists" />
-      }
-    >
-      <EditableRefdataCategoryList
-        displayConditions={displayConditions}
-        label={
-          <FormattedMessage id="ui-licenses.settings.pickLists" />
-        }
-        refdataEndpoint={REFDATA_ENDPOINT}
-      />
-    </Pane>
+      refdataEndpoint={REFDATA_ENDPOINT}
+    />
   );
 };
 
