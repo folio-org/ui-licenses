bcd991357f941ea887b6686f6513368d9df5bb2a
Monireh Rasouli <83644557+MonireRasouli@users.noreply.github.com>
2025-05-14 15:30:25 +0100

    ERM-3704, Hide tags icon when tags are disabled (#783)

--- src/hooks/index.js
+++ src/hooks/index.js
@@ -1,3 +1,2 @@
 export { default as useLicensesContexts } from './useLicensesContexts';
-export { default as useLicensesHelperApp } from './useLicensesHelperApp';
 export { default as useLicenseRefdata } from './useLicenseRefdata';
